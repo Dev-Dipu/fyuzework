c4dfe535c1125b7d36f96746eebd68f6f423f5dd
Anuj Meena Sharma <anuj.shar2k8@gmail.com>
2025-11-01 15:42:13 +0530

    changes are done

--- app/about/sections/Dominate.js
+++ app/about/sections/Dominate.js
@@ -5,10 +5,9 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { SplitText } from "gsap/SplitText";
 
 const Dominate = () => {
+  
   gsap.registerPlugin(ScrollTrigger, SplitText);
-
   const headingRef = useRef(null);
-
   useEffect(() => {
     const heading = headingRef.current;
 
@@ -53,11 +52,10 @@ const Dominate = () => {
   }, []);
 
   return (
-    <div className='h-screen w-full relative font-archivo'>
+    <div className='h-fit pb-42 w-full relative font-archivo'>
       <h1 
         ref={headingRef}
-        className='text-2xl sm:text-3xl md:text-3xl lg:text-4xl font-bold leading-tight text-center pt-24 sm:pt-28 md:pt-32 lg:pt-40 px-4 sm:px-6 md:px-10 lg:px-16'
-      >
+        className='text-2xl sm:text-3xl md:text-3xl lg:text-4xl font-bold leading-tight text-center pt-24 sm:pt-28 md:pt-32 lg:pt-40 px-4 sm:px-6 md:px-10 lg:px-16'>
         No matter your size, Fyuze.ai gives you <br /> the tools to dominate influencer <br /> marketing.
       </h1>
     </div>
